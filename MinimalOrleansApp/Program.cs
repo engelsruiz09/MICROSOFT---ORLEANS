2500b800e0ddbf7f9f77831359a20a2cd74a8a98
engelsruiz09 <jaruizc@correo.url.edu.gt>
2024-02-02 18:57:58 -0600

    fin de demo

---
+++ MinimalOrleansApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace OrleansHelloWorld
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            IHostBuilder builder = Host.CreateDefaultBuilder(args)
+                .UseOrleans(silo =>
+                {
+                    silo.UseLocalhostClustering()
+                        .ConfigureLogging(logging => logging.AddConsole());
+                })
+                .UseConsoleLifetime();
+
+            using IHost host = builder.Build();
+
+            await host.RunAsync();
+        }
+    }
+}
